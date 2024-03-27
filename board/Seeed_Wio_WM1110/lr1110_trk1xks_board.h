12eef4dd46f42f6e15448b5d8ae75fb9a8645e8f
Phil Erwin <pderwin2003@yahoo.com>
2024-03-27 09:45:23 -0400

    lr1110_trk1xks_board: Need to remove this file

--- board/Seeed_Wio_WM1110/lr1110_trk1xks_board.h
+++ board/Seeed_Wio_WM1110/lr1110_trk1xks_board.h
@@ -14,7 +14,6 @@ typedef enum
 #define TRACKER_BOARD_BATTERY_CAPACITY 2400
 
 void     smtc_board_stop_periodic_led_pulse( void );
-uint32_t smtc_board_read_busy_pin( void *context );
 void     smtc_board_select_gnss_antenna(uint32_t which);
 void     smtc_board_reset_radio( void *context );
 void     smtc_board_hall_effect_enable_for_duration( uint32_t duration);
